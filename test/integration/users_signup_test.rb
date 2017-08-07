5d88542d8abfbc60b0996c568a2e3fd42377fc9b
Mhretaab <ermu96@gmail.com>
2017-08-07 19:33:53 +0300

    configured for production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,12 +18,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "valid signup information" do
     get signup_path
+    assert_response :success
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, params: {user: {name: "Example User",
+      post users_path, params: {user: {name: "Example User",
                                                     email: "user@example.com",
                                                     password: "password",
                                                     password_confirmation: "password"}}
     end
+    follow_redirect!
+    assert_response :success
     assert_template 'users/show'
   end
 
